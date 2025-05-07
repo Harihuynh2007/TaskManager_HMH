b003b6671ef4e726211ee545c27f3d96183aa0ae
Harihuynh2007 <hminhhai2000@gmail.com>
2025-05-07 23:56:11 +0700

    update View

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -7,3 +7,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('email', 'username', 'password', ' id')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user   
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(**attrs)
+        if user:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from rest_framework import generics , status
+from rest_framework.response import Response
+from .serializer import RegisterSerializer, LoginSerializer
+
 # Create your views here.
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": serializer.data,
+            "message": "User created successfully"
+        }, status=status.HTTP_201_CREATED)
+    
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        })
+
